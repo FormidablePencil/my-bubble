2b51994ab1393f7f00eec55fa195c5a0d2604c88
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2020-11-22 15:54:22 -0800

    changed fetching data from server hosted on heroku to one hosted on google cloud platform

--- src/actions/fetchAllPortfolioData.ts
+++ src/actions/fetchAllPortfolioData.ts
@@ -1,7 +1,7 @@
 import { FETCHED_ALL_PROJECT_DATA, FETCHED_ALL_TECH_DATA } from './types'
 
 const fetchAllProjectData = () => async (dispatch) => {
-  const fetchedData = await fetch('http://10.0.0.7:8001/retrieveAllData')
+  const fetchedData = await fetch('https://vibrant-ring-296422.wl.r.appspot.com/retrieveAllData')
   if (fetchedData.status === 404) return
   let data = await fetchedData.json()
   dispatch({ type: FETCHED_ALL_PROJECT_DATA, payload: data.projectCollection })
