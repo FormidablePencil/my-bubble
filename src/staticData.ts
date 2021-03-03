5012d4806074ec8d883170cb707eda887cf01248
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2021-03-02 22:26:03 -0800

    fixed bg color

--- src/staticData.ts
+++ src/staticData.ts
@@ -1,21 +1,33 @@
 const staticData = {
   homeAbout: {
-    name: 'Dennis Aleksandrov',
+    name: "Dennis Aleksandrov",
     paragraph: "I develop apps for mobile and the web",
-    paragraph2: 'Full Stack Developer',
-  }
-}
+    paragraph2: "Full Stack Developer",
+  },
+};
 
 export const sortProjects = (projectsArr) => {
-  const orderOfProjects = ['Movie App', 'My Bubble', "Pao System", 'Emoji Tack Toes', 'Simple Pokedex', 'Crackalackin', "Cinematographer's portfolio", 'Server for Todolist', 'Starbucks Replica', 'Portfolio']
-  let sorted: any = []
+  const orderOfProjects = [
+    "TR Legacy",
+    "Movie App",
+    "My Bubble",
+    "Pao System",
+    "Emoji Tack Toes",
+    "Simple Pokedex",
+    "Crackalackin",
+    "Cinematographer's portfolio",
+    "Server for Todolist",
+    "Starbucks Replica",
+    "Portfolio",
+  ];
+  let sorted: any = [];
 
-  orderOfProjects.forEach(orderedItem => {
-    const [filtered] = projectsArr.filter(item => item.title === orderedItem)
-    if (filtered) sorted.push(filtered)
+  orderOfProjects.forEach((orderedItem) => {
+    const [filtered] = projectsArr.filter((item) => item.title === orderedItem);
+    if (filtered) sorted.push(filtered);
   });
 
-  return sorted
-}
+  return sorted;
+};
 
-export default staticData
+export default staticData;
